c563d3ea307c0764f99bfe34c9089f4ee6f90744
Vitor Pereira <vitor@blong.com.br>
2024-04-03 16:41:15 -0300

    First
    First

--- src/views/base/cards/Cards.js
+++ src/views/base/cards/Cards.js
@@ -22,6 +22,7 @@ import {
 import { DocsExample } from 'src/components'
 
 import ReactImg from 'src/assets/images/react.jpg'
+import WidgetsDropdown from '../../widgets/WidgetsDropdown'
 
 const Cards = () => {
   return (
@@ -31,6 +32,7 @@ const Cards = () => {
           <CCardHeader>
             <strong>Card</strong> <small>Example</small>
           </CCardHeader>
+          <WidgetsDropdown className="mb-4" />
           <CCardBody>
             <p className="text-body-secondary small">
               Cards are built with as little markup and styles as possible but still manage to
