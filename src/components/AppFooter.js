201f9929b17bd9bddb4c622500463f35b46718b5
Vitor Pereira <vitor@blong.com.br>
2024-04-03 17:56:37 -0300

    Changes
    Changes

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -5,16 +5,12 @@ const AppFooter = () => {
   return (
     <CFooter className="px-4">
       <div>
-        <a href="https://coreui.io" target="_blank" rel="noopener noreferrer">
-          CoreUI
+        <a href="https://www.abbint.net/" target="_blank" rel="noopener noreferrer">
+          Abb International Inc
         </a>
-        <span className="ms-1">&copy; 2024 creativeLabs.</span>
       </div>
       <div className="ms-auto">
-        <span className="me-1">Powered by</span>
-        <a href="https://coreui.io/react" target="_blank" rel="noopener noreferrer">
-          CoreUI React Admin &amp; Dashboard Template
-        </a>
+        <span className="ms-1">&copy; {new Date().getFullYear()} All Rights Reserved.</span>
       </div>
     </CFooter>
   )
