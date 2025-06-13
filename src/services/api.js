1209750358cf088714e875fe948c22e72b6b7a67
Vitor Pereira <vitor@blong.com.br>
2025-06-13 16:16:08 -0300

    Fix Token
    Fix Token

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ const api = axios.create({
   baseURL: "https://api.blongtech.com/",
 });
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c2VyIjoxLCJlbWFpbCI6InZpdG9yQGFiYmludC5uZXQiLCJpYXQiOjE3MTI2MDM0OTcsImV4cCI6MTcxMjYwNzA5N30.FufGrhx3YwOG2mEOWcggcyWYG91EzgSKJEhQSvzX_c4';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZF91c2VyIjoxLCJlbWFpbCI6InZpdG9yQGFiYmludC5uZXQiLCJpYXQiOjE3NDk4NDIyMTMsImV4cCI6MTc0OTg0NTgxM30.TZW2YAtzAoS-FVaBo7DIBz-P7xJ4YewTtKrjydKYESw';
 
 if (token) {
   api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
